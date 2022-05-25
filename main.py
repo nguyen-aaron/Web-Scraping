f027d13ddb1a0eaf1c97c37003622877245f0ec6
Aaron Nguyen <103400664+nguyen-aaron@users.noreply.github.com>
2022-05-25 18:29:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+def getValue(url):
+  page = requests.get(url) #get page
+
+  soup = BeautifulSoup(page.content, "html.parser") #scrape for content
+
+  #results = soup.find("fin-streamer", class_ = "Fw(b) Fz(36px) Mb(-4px) D(ib)")
+  #print(results.prettify())
+  #results = soup.find("h1", class_ = "D(ib) Fz(18px)")
+  #print(results.prettify())
+
+  findNameOfItem = [a.text for a in soup.select_one('h1')] #find the text in specific h1
+  stringOfItem = str(findNameOfItem) #convert to string
+  index = stringOfItem.index("(") #use indexing to find the end of name of item
+  nameOfItem = stringOfItem[2:index -1] #clean up name of item, get rid of outer parenthesis and quotes
+  valueOfItem = soup.select_one('fin-streamer[data-test="qsp-price"]')["value"]
+  print("The price of",nameOfItem,"is",valueOfItem) #print out name and value
+
+#examples
+getValue("https://finance.yahoo.com/quote/BTC-USD/")
+getValue("https://finance.yahoo.com/quote/GOOG/")
+getValue("https://finance.yahoo.com/quote/%5EGSPC?p=%5EGSPC")
+getValue("https://finance.yahoo.com/quote/GME?p=GME&.tsrc=fin-srch")
